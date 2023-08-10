b4fa84264680a90592455b2292a89547a1182f83
Soham Shinde <56295513+sohamthirty@users.noreply.github.com>
2023-08-10 10:43:04 -0400

    Time: 962 ms (43.86%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-#  CASE 
-#     WHEN num IS NULL THEN SELECT 'null'
-#     ELSE SELECT num END
-SELECT CASE WHEN COUNT(NUM)=1 THEN num
-            ELSE NULL END as num
+SELECT IF(COUNT(NUM)=1, num, NULL) as num
 FROM MyNumbers
 GROUP BY num
 ORDER BY num DESC
