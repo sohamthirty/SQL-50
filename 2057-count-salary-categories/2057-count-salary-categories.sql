f57ad02483a19ad6a9bdeb55ae3a54d4fe754e0f
Soham Shinde <56295513+sohamthirty@users.noreply.github.com>
2023-08-20 14:29:48 -0400

    Time: 2944 ms (26.26%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,16 +1,5 @@
-# SELECT COUNT(income<20000), COUNT(income>=20000 AND income<=50000), COUNT(income>50000)
-SELECT c.category, COUNT(a.income) as accounts_count
-FROM
-( 
-  SELECT 'Low Salary' as category
-  UNION ALL
-  SELECT 'Average Salary' as category
-  UNION ALL
-  SELECT 'High Salary' as category
-) as c
-LEFT JOIN Accounts as a
-ON(
-  (c.category='Low Salary' AND a.income<20000) OR
-  (c.category='Average Salary' AND a.income>=20000 AND a.income<=50000) OR
-  (c.category='High Salary' AND a.income>50000))
-GROUP BY c.category
+select 'Low Salary' as category, count(account_id) as accounts_count from accounts where income <20000
+union
+select 'Average Salary' as category, count(account_id) as accounts_count from accounts where income between 20000 and 50000
+union
+select 'High Salary' as category, count(account_id) as accounts_count from accounts where income>50000
